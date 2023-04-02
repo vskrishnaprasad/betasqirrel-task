e91983a6d72d17c94b080be00871fab2f01b3322
kpmakonics <krishnaprasadvs12@gmail.com>
2023-04-02 12:23:00 +0530

    task-7 chaged, task-6 icon color change

--- task-7/script.js
+++ task-7/script.js
@@ -1,11 +1,58 @@
-function display(value){
-    document.getElementById("screen").value += value;
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.btn');
+let firstOperand = '';
+let operator = '';
+let secondOperand = '';
+let result = '';
+
+function updateDisplay(value) {
+  display.textContent = value;
 }
-function clearscreen(){
-    document.getElementById("screen").value = "";
+
+function clear() {
+  firstOperand = '';
+  operator = '';
+  secondOperand = '';
+  result = '';
+  updateDisplay('');
 }
-function calculate(){
-    var a =  document.getElementById("screen").value;
-    var b = eval(a);
-    document.getElementById("screen").value =b;
+
+function calculate() {
+  let num1 = parseFloat(firstOperand);
+  let num2 = parseFloat(secondOperand);
+  if (operator === '+') {
+    result = num1 + num2;
+  } else if (operator === '-') {
+    result = num1 - num2;
+  } else if (operator === 'x') {
+    result = num1 * num2;
+  } else if (operator === '/') {
+    result = num1 / num2;
+  }
+  updateDisplay(result);
+  firstOperand = result;
+  operator = '';
+  secondOperand = '';
 }
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const value = button.getAttribute('value');
+    if (value === 'C') {
+      clear();
+    } else if (value === '+' || value === '-' || value === 'x' || value === '/') {
+      operator = value;
+      updateDisplay('');
+    } else if (value === '=') {
+      calculate();
+    } else {
+      if (!operator) {
+        firstOperand += value;
+        updateDisplay(firstOperand);
+      } else {
+        secondOperand += value;
+        updateDisplay(secondOperand);
+      }
+    }
+  });
+});
